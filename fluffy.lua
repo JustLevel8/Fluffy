2012d69864fba0a7c9e35ec93da2685d30dcbf90
JustLevel8 <100040340+JustLevel8@users.noreply.github.com>
2022-06-07 21:43:16 +0200

    Update fluffy.lua

--- fluffy.lua
+++ fluffy.lua
@@ -97,13 +97,13 @@ function HideGui()
         end
     end
     if k.Enabled then
-        l:TweenSize(UDim2.new(0, 0, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .6, true)
+        l:TweenSize(UDim2.new(0, 100, 0, 0), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .6, true)
         repeat
             task.wait()
         until l.Size.Y.Offset < 1
         k.Enabled = false
     else
-        l:TweenSize(UDim2.new(0, 700, 0, 344), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .6, true)
+        l:TweenSize(UDim2.new(0, 615, 0, 344), Enum.EasingDirection.Out, Enum.EasingStyle.Quart, .6, true)
         repeat
             task.wait()
         until l.Size.Y.Offset > 1
